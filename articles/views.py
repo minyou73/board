a046d8930e3d4cc789e829b83034f5b9908bcedd
minyou73 <145949811+minyou73@users.noreply.github.com>
2023-11-02 15:32:39 +0900

    board

--- articles/views.py
+++ articles/views.py
@@ -46,3 +46,22 @@ def delete(request, id):
     article.delete()
 
     return redirect('articles:index')
+
+def edit(request, id):
+    article = Article.objects.get(id=id)
+
+    context = {
+        'article': article,
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    #새로운 정보
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    #기존정보
+    article = Article.objects.get(id=id)
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', id=article.id)
